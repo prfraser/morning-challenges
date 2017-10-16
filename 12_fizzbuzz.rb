aee968095abf0c4f8dcfa69c7ecd6917e1805be6
philip fraser <pr.fraser3@gmail.com>
2017-10-16 10:13:14 +1000

    challenge12

---
+++ 12_fizzbuzz.rb
@@ -0,0 +1,49 @@
+# FizzBuzz
+#
+# Don't look this one up until you complete it yourself!
+# It's a common interview question and there will be plenty
+# of spoilers on the interwebs.
+#
+# This is a trivial question with many simple solutions.
+# Try to write the least amount of code as possible.
+# (AKA: Travisify™ your solution).
+#
+# Write a program that prints the numbers from 1 to 100.
+# But for multiples of three print “Fizz” instead of the
+# number and for the multiples of five print “Buzz”. For
+# numbers which are multiples of both three and five
+# print “FizzBuzz”.
+#
+# Difficulty:
+# 4/10
+#
+# Example:
+# 1
+# 2
+# Fizz
+# 4
+# Buzz
+# ...etc
+#
+
+# Your code here  
+# def fizzbuzz
+# 	(1..100).each do |n| 
+# 		if n % 15 == 0
+# 			puts "FizzBuzz" 
+# 		elsif n % 5 == 0 
+# 			puts "Buzz"
+# 		elsif n % 3 == 0 
+# 			puts "Fizz"
+# 		else puts n 
+# 		end
+# 	end
+# end
+
+
+# fizzbuzz
+(1..100).each do |x|
+  fizz = "Fizz" if (x % 3 == 0)
+  buzz = "Buzz" if (x % 5 == 0)
+  puts (fizz || buzz) ? fizz.to_s + buzz.to_s : x
+end
