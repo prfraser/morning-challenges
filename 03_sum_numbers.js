eb79b11625e76c6d89d4e242e55ee432e68f9c16
philip fraser <pr.fraser3@gmail.com>
2017-11-11 19:51:40 +1000

    03 js challenge

---
+++ 03_sum_numbers.js
@@ -0,0 +1,16 @@
+// # Sum Numbers
+// #
+// # Write a method that will take an array of numbers,
+// # and return their sum.
+// #
+// # Difficulty:
+// # 3/10
+// #
+// # Example:
+// # sum_numbers([1,1,1]) should return 3
+// # sum_numbers([5,2,100,0,10]) should return 117
+
+var sum_numbers = (array) => array.reduce((total, value) => { return total + value; }, 0);
+
+console.log(sum_numbers([1,1,1]));
+console.log(sum_numbers([5,2,100,0,10]));
